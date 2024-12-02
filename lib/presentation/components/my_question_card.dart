5711300c987dc3c2368a5c6d2b57f7bd380883fd
Asus <turgunboyevjurabek777@gmail.com>
2024-12-02 23:21:50 +0500

    aaa

--- lib/presentation/components/my_question_card.dart
+++ lib/presentation/components/my_question_card.dart
@@ -4,11 +4,13 @@ class MyQuestionCard extends StatelessWidget {
   final String questionText;
   final bool isCorrect;
   final bool isSelected;
+  final Color color;
   final VoidCallback onTap;
 
   MyQuestionCard({
     required this.questionText,
     required this.isCorrect,
+    required this.color,
     required this.isSelected,
     required this.onTap,
   });
@@ -18,19 +20,25 @@ class MyQuestionCard extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: SizedBox(
-        width: double.infinity, // Kartaning kengligi ekranga to'liq mos bo'ladi
-        child: Card(
-          color: isCorrect ? Colors.green[100] : Colors.red[100],
-          elevation: 4,
-          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              questionText,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(
+                border: Border.all(width: 1.5),
+                color: color,
+                borderRadius: BorderRadius.circular(20),
+                  ),
+            padding: EdgeInsets.all(3),
+            child: Center(
+              child: Text(
+                questionText,
+                style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,            
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
         ),
--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -11,13 +11,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int questionsCount = 0;
-  bool isSelected = false;
+  String? selectedAnswer; // Tanlangan javobni saqlash uchun
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 3.0,
+        elevation: 10.0,
         centerTitle: true,
         shadowColor: Colors.black,
         backgroundColor: Colors.white,
@@ -29,13 +29,14 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 10),
+        
+            // Savol nomeri
+            Container(
               margin: const EdgeInsets.only(top: 10),
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -50,92 +51,137 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
               child: Padding(
-                padding: EdgeInsets.all(7),
+                padding: const EdgeInsets.all(5),
                 child: Text(
                   "Savol: ${questionsCount + 1}",
                   style: const TextStyle(
-                      fontSize: 15, fontWeight: FontWeight.bold),
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              )),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromARGB(221, 0, 0, 0),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: Offset(8, 10),
-                  ),
-                ],
-              ),
-              child: Text(
-                questions[questionsCount].questionText,
-                style:
-                    const TextStyle(fontSize: 15, fontWeight: FontWeight.w900),
               ),
             ),
-          ),
-          const SizedBox(height: 15),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Container(
-              padding: EdgeInsets.all(7),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black,
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: Offset(8, 10),
+            const SizedBox(height: 10),
+            // Savol
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: Container(
+                alignment: Alignment.center,
+                height: 100,
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color.fromARGB(221, 0, 0, 0),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(8, 10),
+                    ),
+                  ],
+                ),
+                child: Text(
+                  questions[questionsCount].questionText,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
                   ),
-                ],
+                ),
               ),
-              child: Column(
-                children: questions[questionsCount].answers.map((answer) {
-                  return MyQuestionCard(
-                    questionText: answer,
-                    isSelected: isSelected,
-                    isCorrect: answer ==
+            ),
+            const SizedBox(height: 15),
+        
+            // Javoblar
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(
+                padding: const EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(8, 10),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: questions[questionsCount].answers.map((answer) {
+                    bool isCorrect = answer ==
                         questions[questionsCount].answers[
-                            questions[questionsCount].correctAnswerIndex],
-                    onTap: () {
-                      if (questionsCount < questions.length - 1) {
-                        setState(() {
-                          isSelected = true;
-                        });
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            showCloseIcon: true,
-                            content: Text("Savollar tugadi :)"),
-                          ),
-                        );
+                            questions[questionsCount].correctAnswerIndex];
+        
+                    // Rangni hisoblash
+                    Color cardColor = Colors.transparent;
+                    if (selectedAnswer != null) {
+                      if (answer == selectedAnswer) {
+                        cardColor = isCorrect ? Colors.amber : Colors.red;
+                      } else if (isCorrect) {
+                        cardColor = Colors.amber;
                       }
-                    },
-                  );
-                }).toList(),
+                    }
+        
+                    return MyQuestionCard(
+                      questionText: answer,
+                      isSelected: selectedAnswer == answer,
+                      isCorrect: isCorrect,
+                      color: cardColor,
+                      onTap: () {
+                        if (selectedAnswer == null) {
+                          setState(() {
+                            selectedAnswer = answer; // Tanlangan javobni saqlash
+                          });
+                        }
+                      },
+                    );
+                  }).toList(),
+                ),
               ),
             ),
-          ),
-         const SizedBox(),
-          ElevatedButton(onPressed: (){
-                    if(isSelected){
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+              child: SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.black,
+                    elevation: 15,
+                    backgroundColor: selectedAnswer != null ? Colors.orangeAccent : Colors.grey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (selectedAnswer != null) {
                       setState(() {
-                        questionsCount++;
-                        isSelected = false;
+                        if (questionsCount < questions.length - 1) {
+                          questionsCount++;
+                          selectedAnswer = null; // Yangi savol uchun tozalash
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              showCloseIcon: true,
+                              content: Text("Savollar tugadi :)"),
+                            ),
+                          );
+                        }
                       });
                     }
-          },
-           child: const Text("Keyingi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),))
-        ],
+                  },
+                  child: const Text(
+                    "Keyingi",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
